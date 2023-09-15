e97b0c1111199b8c844b7ba1247fe996cd0b7870
Govindsing Chavan <123160733+govind2611@users.noreply.github.com>
2023-09-15 09:41:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,7 +9,35 @@ const enrollmentData = {
   ],
 };
 
-// complete this function
-async function calculateEnrollment() {}
+// Simulate fetching data from an API asynchronously
+async function fetchEnrollmentData() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(enrollmentData.enrollmentData);
+    }, 1000); // Simulated delay of 1 second (you can replace this with your actual API fetch)
+  });
+}
+
+// Calculate enrollment asynchronously
+async function calculateEnrollment() {
+  const enrollmentData = await fetchEnrollmentData();
+
+  const courseEnrollment = {};
+
+  enrollmentData.forEach((entry) => {
+    const { student, course } = entry;
+
+    if (courseEnrollment[course]) {
+      courseEnrollment[course]++;
+    } else {
+      courseEnrollment[course] = 1;
+    }
+  });
+
+  // Log the list of courses and their total enrollment numbers
+  for (const course in courseEnrollment) {
+    console.log(`Course: ${course}, Total Enrollment: ${courseEnrollment[course]}`);
+  }
+}
 
 calculateEnrollment();
